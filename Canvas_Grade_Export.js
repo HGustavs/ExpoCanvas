db33334800e23049efe79816a4877947507aa15b
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2021-08-23 20:53:18 +0200

    Data generation w column names

--- Canvas_Grade_Export.js
+++ Canvas_Grade_Export.js
@@ -33,6 +33,8 @@ function makebutton()
             }            
         }
 
+        var thedata=[];
+
         // Read headers
         var sven = document.getElementsByClassName("headers_1");
         var theaders=[];
@@ -41,24 +43,37 @@ function makebutton()
             for (var i=0; i<test.length; i++) {
                 var assignmentid=test[i].className.substr(test[i].className.indexOf("_")+1);
                 var title=test[i].title;
-                // console.log(assignmentid+" "+title);
+                theaders["a"+assignmentid]=title;
             }            
         }
 
         // Read data
         var sven = document.getElementsByClassName("viewport_1");
-        var thedata=[];
         for(var j=0;j<sven.length;j++){
             var test = sven[j].getElementsByClassName("slick-row");
             for (var i=0; i<test.length; i++) {
+                // All other rows setup
                 studid=test[i].className.substr(test[i].className.indexOf("_")+1);
                 var grades=test[i].getElementsByClassName("assignment");
+
+                // Before first row generate headings
+                if(i==0){
+                    var outgrades=[];
+                    outgrades.push("Student");
+                    for (var k=0; k<grades.length; k++) {
+                      var assignmentid=grades[k].className.substr(grades[k].className.indexOf("_")+1);
+                      outgrades.push(theadeers["a"+assignmentid];
+                    }
+                    console.log(outgrades);
+                }
+
+                // All other rows generate data
                 var outgrades=[];
                 outgrades.push(stud["s"+studid]);
                 for (var k=0; k<grades.length; k++) {
-                    var assignmentid=grades[k].className.substr(grades[k].className.indexOf("_")+1);
-                    var grade=grades[k].innerText;
-                    outgrades.push({assignment:assignment,grade:grade});
+                  var assignmentid=grades[k].className.substr(grades[k].className.indexOf("_")+1);
+                  var grade=grades[k].innerText;
+                  outgrades.push({assignment:assignmentid, grade:grade});
                 }
                 console.log(outgrades);
             }                          
